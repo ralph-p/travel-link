db80419e137cfe1f163e3cf4ae42e48a89d4acbf
ralph-p <mr.pereira91@gmail.com>
2023-06-07 14:47:29 -0400

    skeleton for coifig

--- pages/config.js
+++ pages/config.js
@@ -1,9 +1,28 @@
 import React from 'react'
-
+import { Tabs } from 'flowbite-react';
 const Config = () => {
   return (
     <div className="container mx-auto">
-        Config Page will go here
+      <Tabs.Group
+        aria-label="Tabs with underline"
+        style="fullWidth"
+      >
+        <Tabs.Item title={"Vendors"} >
+          <div>Vendor List</div>
+        </Tabs.Item>
+        <Tabs.Item title={"Dashboard"}>
+          <div>Dashboard</div>
+
+        </Tabs.Item>
+        <Tabs.Item title={"Invoices"}>
+          <div>List of invoices Details</div>
+
+        </Tabs.Item>
+        <Tabs.Item title={"Account"}>
+          <div>Account Details</div>
+
+        </Tabs.Item>
+      </Tabs.Group>
     </div>
   )
 }
