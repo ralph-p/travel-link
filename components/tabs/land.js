a495accdca9d844fa652bfbd665db58bd777bdba
ralph-p <mr.pereira91@gmail.com>
2023-06-07 12:01:33 -0400

    fixed build errors

--- components/tabs/land.js
+++ components/tabs/land.js
@@ -9,7 +9,7 @@ export const Land = () => {
         <select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
           {
             landTransportOptions.map(({ value, option }) => (
-              <option value={value}>{option}</option>
+              <option key={`land-${value}`} value={value}>{option}</option>
             ))
           }
         </select>
--- components/tabs/sea.js
+++ components/tabs/sea.js
@@ -9,7 +9,7 @@ export const Sea = () => {
         <select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
           {
             seaTransportOptions.map(({ value, option }) => (
-              <option value={value}>{option}</option>
+              <option key={`sea-${value}`} value={value}>{option}</option>
             ))
           }
         </select>
