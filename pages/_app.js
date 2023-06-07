a26a9456f670228ec19ccaccea62e480acb129a0
ralph-p <mr.pereira91@gmail.com>
2023-06-07 15:10:46 -0400

    added vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,22 @@
 import { NavBar } from '@/components/nav'
 import '@/styles/globals.css'
 import { useEffect } from 'react';
-
+import { Analytics } from '@vercel/analytics/react';
 export default function App({ Component, pageProps }) {
   useEffect(() => {
     document.documentElement.classList.add('dark');
-    
+
   }, [])
-  
-  return <div>
 
-    <NavBar />
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 w-full bg-blue-300`}
-    >
-
-      <Component {...pageProps} />
-    </main>
-  </div>
+  return (
+    <div>
+      <NavBar />
+      <main
+        className={`flex min-h-screen flex-col items-center justify-between p-24 w-full bg-blue-300`}
+      >
+        <Component {...pageProps} />
+      </main>
+      <Analytics />
+    </div>
+  )
 }
